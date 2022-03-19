ac5fe3085997a00c93b94bf40f5d9e6133a934e6
Rodrigo Mattiello <didimartinez2011@hotmail.com>
2022-03-19 14:40:47 -0300

    Mudança login

--- src/views/SellerLoginPage.vue
+++ src/views/SellerLoginPage.vue
@@ -1,27 +1,31 @@
 <template>
   <v-container class="login-page">
-    <h2>Login</h2>
-    <v-form ref="form" v-model="valid" lazy-validation>
-      <v-text-field
-        v-model="email"
-        :rules="emailRules"
-        label="E-mail"
-      ></v-text-field>
+    <div class="login">
+      <h2 class="ml-2">Login</h2>
+      <v-form ref="form" v-model="valid" lazy-validation>
+        <v-text-field class="ml-2 mr-2" 
+          v-model="email"
+          :rules="emailRules"
+          label="E-mail"
+        ></v-text-field>
 
-      <v-text-field
-        v-model="email"
-        :rules="emailRules"
-        label="Senha"
-      ></v-text-field>
+        <v-text-field class="ml-2 mr-2" 
+          v-model="email"
+          :rules="emailRules"
+          label="Senha"
+        ></v-text-field>
 
-      <router-link to="/">
-        <v-btn color="success" class="mr-4" @click="validate"> Login </v-btn>
-      </router-link>
+        <router-link to="/">
+          <v-btn color="success" class="mr-4 ml-2" @click="validate"> Login </v-btn>
+        </router-link>
 
-      <router-link to="/cadastro">
-        <v-btn color="" @click="resetValidation"> Cadastre-se </v-btn>
-      </router-link>
-    </v-form>
+        <router-link to="/cadastro">
+          <v-btn color="" @click="resetValidation"> Cadastre-se </v-btn>
+        </router-link>
+      </v-form>
+
+      <v-checkbox v-model="checkbox" :label="'Manter-me logado'" class="ml-2"></v-checkbox>
+    </div>
   </v-container>
 </template>
 
@@ -32,6 +36,7 @@ export default {
 </script>
 
 <style scoped>
+
 .login-page a {
   text-decoration: none;
 }
