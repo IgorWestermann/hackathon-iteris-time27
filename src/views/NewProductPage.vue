fe95c2d55ff3201041c97463ac2ff3ca84e7fb65
Letícia Campos <scleticiaa@gmail.com>
2022-03-19 16:55:12 -0300

    adição da API

--- src/views/NewProductPage.vue
+++ src/views/NewProductPage.vue
@@ -7,34 +7,6 @@
                 v-model="valid"
                 lazy-validation
             >
-            
-            <v-file-input
-                label="Imagem"
-                filled
-                prepend-icon="mdi-camera"
-            ></v-file-input>
-
-            <p class="mb-2 mt-6">Local:</p>
-                <v-text-field
-                :rules="Rules"
-                label="Nome do Estabelecimento"
-                required
-                ></v-text-field>
-
-                <v-text-field
-                :rules="Rules"
-                label="CEP"
-                required
-                ></v-text-field>
-
-                
-                <v-text-field
-                :rules="Rules"
-                label="Endereço"
-                required
-                ></v-text-field>
-
-                <p class="mb-2 mt-6">Produto:</p>
 
                 <v-text-field
                 :rules="Rules"
@@ -55,6 +27,12 @@
                 required
                 ></v-text-field>
 
+                <v-file-input class= mt-4
+                label="Imagem"
+                filled
+                prepend-icon="mdi-camera"
+            ></v-file-input>
+
                 <v-btn @click="validate" rounded color="deep-purple lighten-2" dark> Validar </v-btn>
 
             </v-form>
@@ -66,16 +44,16 @@
                 <template v-slot:default>
                 
                 <tbody>
-                    <tr
-                    v-for="item in desserts"
-                    :key="item.name"
-                    >
-                    <v-avatar color="deep-purple lighten-2" dark class="mt-2" size ="34">
-                   
+                    <tr v-for="(item, i) in produtosRegiaoAP" :key="i">
+                    <v-avatar dark size="30" class="mt-5 ml-2">
+                      <img
+                          :src="produtosRegiaoAP[i].imagem"
+                          :alt="produtosRegiaoAP[i].nome"
+                      >
                     </v-avatar>
-                    <td>{{ item.nome }}</td>
-                    <td>{{ item.preco }}</td>
-                    <td>{{ item.sabor }}</td>
+                    <td class="mb-4 pb-3 pt-3">{{ produtosRegiaoAP[i].nome }}</td>
+                    <td class="mb-4 pb-3 pt-3">R${{ produtosRegiaoAP[i].preco }}</td>
+                    <td class="mb-4 pb-3 pt-3">{{ produtosRegiaoAP[i].sabor }}</td>
                     </tr>
                 </tbody>
                 </template>
@@ -88,37 +66,43 @@
 export default {
     name: 'NewProductPage',
    data: () => ({
-        desserts: [
-          {
-            nome: 'Ovo1',
-            preco: 'R$50,00',
-            sabor: 'chocolate ao leite',
-          },
-          {
-            nome: 'Ovo2',
-            preco: 'R$70,00',
-            sabor: 'chocolate branco',
-          },
-        ],
+      tab: null,
+      produtos: [],
+      produtosRegiaoAM: [],
+      produtosRegiaoAC: [],
+      produtosRegiaoAP: [],
+      items: ["Manaus", "Rio Branco", "Macapá"],
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       valid: true,
       rules: '',
       Rules: [
         v => !!v || 'Campo Inválido',
       ],
-      select: null,
-      items: [
-        'Item 1',
-        'Item 2',
-        'Item 3',
-        'Item 4',
-      ],
-      checkbox: false,
     }),
 
     methods: {
       validate () {
-        this.$refs.form.validate()
+        this.$refs.form.validate();
+       this.$refs.form ='';
       },
     },
+    created() {
+    fetch("https://it3-hbn-default-rtdb.firebaseio.com/ovosPascoa.json")
+      .then((res) => res.json())
+      .then((json) => {
+        this.produtos = json;
+      })
+      .then(() => {
+        for (let i = 0; i < this.produtos.length; i++) {
+          if (this.produtos[i].local.id == 2) {
+            this.produtosRegiaoAC.push(this.produtos[i]);
+          } else if (this.produtos[i].local.id == 1) {
+            this.produtosRegiaoAM.push(this.produtos[i]);
+          } else if (this.produtos[i].local.id == 3) {
+            this.produtosRegiaoAP.push(this.produtos[i]);
+          }
+        }
+      });
+    }
   }
 </script>
